e33958a88271227196b61146447bd5cf8265c607
Matheus Teixeira <matheustxr.dev@gmail.com>
2025-06-12 09:31:59 -0300

    feat: created navbar-component and add bootstrap

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Navbar } from "./_components/navbar/navbar";
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Navbar, CommonModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected title = 'gerador-certificado';
+  title = 'gerador-certificado';
+  exibeNavbar: boolean = false;
 }
