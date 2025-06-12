10fd32b30763cd9ab68d4c75d3e051d440d0f7b2
Matheus Teixeira <matheustxr.dev@gmail.com>
2025-06-12 10:37:30 -0300

    feat: created certified-item component

---
+++ src/app/_components/certified-item/certified-item.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { SecondaryButton } from "../secondary-button/secondary-button";
+
+@Component({
+  selector: 'app-certified-item',
+  imports: [SecondaryButton],
+  templateUrl: './certified-item.html',
+  styleUrl: './certified-item.css'
+})
+export class CertifiedItem {
+
+}
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { PrimaryButton } from './_components/primary-button/primary-button';
+import { CommonModule } from '@angular/common';
+import { Navbar } from './_components/navbar/navbar';
+import { SecondaryButton } from "./_components/secondary-button/secondary-button";
+import { CertifiedItem } from "./_components/certified-item/certified-item";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Navbar, CommonModule, PrimaryButton],
+  imports: [RouterOutlet, Navbar, CommonModule, PrimaryButton, SecondaryButton, CertifiedItem],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
