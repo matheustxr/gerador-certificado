01352fda1b42153b96e9e4d3878ed0b7a70a2de8
Matheus Teixeira <matheustxr.dev@gmail.com>
2025-06-12 09:58:30 -0300

    add components and images

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Navbar } from "./_components/navbar/navbar";
+import { CommonModule } from '@angular/common';
+import { PrimaryButton } from "./_components/primary-button/primary-button";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Navbar, CommonModule, PrimaryButton],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected title = 'gerador-certificado';
+  title = 'gerador-certificado';
+  exibeNavbar: boolean = false;
 }
